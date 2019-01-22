441329e7acae33c70f70b8fcec6d30f646f0dfb5
averma35 <averma35@in.ibm.com>
2019-01-22 13:31:06 +0530

    POC on docker

---
+++ springboot-docker/src/main/java/com/docker/SpringDockerHelloApplication.java
@@ -0,0 +1,14 @@
+package com.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerHelloApplication.class, args);
+	}
+
+}
+
