1f50e3726828bfb6b6e1b22f2dca642dfe625935
Amit Verma <amitverma.atpl@gmail.com>
2019-03-24 15:51:43 +0530

    updating the example

--- spring-security-hello/src/main/java/com/ibm/demo/config/SecurityConfig.java
+++ spring-security-hello/src/main/java/com/ibm/demo/config/SecurityConfig.java
@@ -28,7 +28,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll();
     }
 
-    @Bean
+    @SuppressWarnings("deprecation")
+	@Bean
     @Override
     public UserDetailsService userDetailsService() {
         UserDetails user =
